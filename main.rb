a03b87807bf92782c3dc665e1c6b13ce7b496998
melagos <mhagos@gatech.edu>
2017-01-21 13:18:01 -0500

    Added CSS file and completed HTML file

--- main.rb
+++ main.rb
@@ -5,8 +5,8 @@ require 'yelp'
   erb :main
 end
 
-client = Yelp::Client.new({ consumer_key: YOUR_CONSUMER_KEY,
-                            consumer_secret: YOUR_CONSUMER_SECRET,
-                            token: YOUR_TOKEN,
-                            token_secret: YOUR_TOKEN_SECRET
-                          })
+# client = Yelp::Client.new({ consumer_key: YOUR_CONSUMER_KEY,
+#                             consumer_secret: YOUR_CONSUMER_SECRET,
+#                             token: YOUR_TOKEN,
+#                             token_secret: YOUR_TOKEN_SECRET
+#                           })
