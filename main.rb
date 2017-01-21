d1f12d2dae7270c363aeadadb9ddef1d94e9eeff
Joseph Hooper <stayvaliant94@gmail.com>
2017-01-21 14:20:57 -0500

    Added forms Linked forms with sinatra backend

--- main.rb
+++ main.rb
@@ -1,12 +1,29 @@
 require 'sinatra'
 require 'yelp'
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-  get '/' do
+get '/' do
   erb :main
 end
 
-client = Yelp::Client.new({ consumer_key: YOUR_CONSUMER_KEY,
-                            consumer_secret: YOUR_CONSUMER_SECRET,
-                            token: YOUR_TOKEN,
-                            token_secret: YOUR_TOKEN_SECRET
+client = Yelp::Client.new({ consumer_key: 	"Z2VUMVUsbQ6rWNcUqRziiA",
+                            consumer_secret: "eJV9557OSLYftsEV2TSGmsja7b0",
+                            token: "GtB4sbw8uZmdoOtNW-iESuFnmMWXUyvl",
+                            token_secret: "JXwLkigBHKq4QCQrLisk644vly0"
                           })
+
+# params = { term: 'food', limit: 3, category_filter: 'discgolf'}
+locale = { lang:'eng' }
+
+# response = client.search('San Franciso', params, locale)
+# puts response.businesses[0].name
+
+# params = {
+#   :name => "Sam",
+#   :favorite_food => "Green Eggs and Ham"
+# }
+
+post '/food' do
+     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+end
