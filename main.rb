6c6c259311d1c3fbb275e1dd3b065592ab26a7c1
Arsene Lakpa <aclak1@yahoo.com>
2017-02-19 21:20:57 -0500

    Added hash to search query

--- main.rb
+++ main.rb
@@ -14,8 +14,8 @@ client = Yelp::Client.new({ consumer_key: 	"Z2VUMVUsbQ6rWNcUqRziiA",
                             token_secret: "JXwLkigBHKq4QCQrLisk644vly0"
                           })
 
- hash = {limit: 5, category_filter: 'food'}
-locale = { lang:'eng' }
+hash = {limit: 5, category_filter: 'food'}
+locale = { lang:'en' }
 
 # response = client.search('San Franciso', params, locale)
 # puts response.businesses[0].name
@@ -32,7 +32,7 @@ post '/food' do
      meal = params[:birth_time]
      stork = params[:stork]
      searchStr = city + " " + name + " " + nationality + " " + meal
-     response = client.search(searchStr)
+     response = client.search(searchStr, hash)
      @business = response.businesses[0].name
      @business_img = response.businesses[0].image_url
      @url = response.businesses[0].url
